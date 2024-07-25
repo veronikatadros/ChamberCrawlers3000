93b4e6624b45d4ffb4aba8c4f5649dec89919d3b
georgia-alpajaro <georgia.alpajaro@gmail.com>
2024-07-25 12:45:25 -0400

    view render method

--- headers/view.h
+++ headers/view.h
@@ -1,10 +1,12 @@
-#include "floor.h"
-#include "humanPlayer.h"
+#include "iostream"
 using namespace std;
 
+class Floor;
+class HumanPlayer;
 class View {
     public:
+        View();
+        virtual ~View();
         string action;
-        void render(const Floor& floor, const HumanPlayer& player);
-
+        void render(const Floor& floor, const HumanPlayer& player, ostream& out = cout);
 };

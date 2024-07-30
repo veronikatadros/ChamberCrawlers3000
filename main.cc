ab362b70decd3e9663bd5d8f909ce3ba40a990a2
georgia-alpajaro <georgia.alpajaro@gmail.com>
2024-07-30 11:53:56 -0400

    toggleable ncurses

--- main.cc
+++ main.cc
@@ -6,30 +6,35 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-    setlocale(LC_ALL, "");
-    initscr();              // Initialize the window
-    raw();                  // Line buffering disabled
-    keypad(stdscr, TRUE);   // Enable special keys capture
-    noecho();               // Don't echo while we do getch
-    start_color();          // Start color functionality
-
-    // Initialize colors if you plan to use them
-    init_pair(1, COLOR_MAGENTA, COLOR_BLACK);       // Example: red text
-    init_pair(2, COLOR_WHITE, COLOR_BLACK);
-    init_pair(3, COLOR_YELLOW, COLOR_BLACK);
-    init_pair(4, COLOR_BLUE, COLOR_BLACK);
-    init_pair(5, COLOR_RED, COLOR_BLACK);
-    init_pair(6, COLOR_CYAN, COLOR_BLACK);
-    init_pair(7, COLOR_BLACK, COLOR_WHITE);
-    init_pair(8, COLOR_GREEN, COLOR_BLACK);
-    init_pair(9, COLOR_WHITE, COLOR_GREEN);
-
-    string command;
-    string dir;
     RandomNumberGenerator();    
     
     string cmd = argc > 1 ? argv[1] : "";
-    Game* game = new Game(cmd);
+    cout << "using ncurses? Y/N";
+    char answer;
+    cin >> answer;
+    Game* game;
+    if (answer == 'Y') {
+        setlocale(LC_ALL, "");
+        initscr();              // Initialize the window
+        raw();                  // Line buffering disabled
+        keypad(stdscr, TRUE);   // Enable special keys capture
+        noecho();               // Don't echo while we do getch
+        start_color();          // Start color functionality
+
+        // Initialize colors if you plan to use them
+        init_pair(1, COLOR_MAGENTA, COLOR_BLACK);       // Example: red text
+        init_pair(2, COLOR_WHITE, COLOR_BLACK);
+        init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+        init_pair(4, COLOR_BLUE, COLOR_BLACK);
+        init_pair(5, COLOR_RED, COLOR_BLACK);
+        init_pair(6, COLOR_CYAN, COLOR_BLACK);
+        init_pair(7, COLOR_BLACK, COLOR_WHITE);
+        init_pair(8, COLOR_GREEN, COLOR_BLACK);
+        init_pair(9, COLOR_WHITE, COLOR_GREEN);
+        game = new Game(cmd, true);
+    } else {
+        game = new Game(cmd, false);
+    }
     game->start();
 
     while(game->playAgain) {
